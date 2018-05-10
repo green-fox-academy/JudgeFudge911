f770df810a13661a8e9b1a70ead35156aeddad9a
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 21:49:06 +0200

    DrawPyramid will get back to this

--- week-01/day-04/src/DrawPyramid.java
+++ week-01/day-04/src/DrawPyramid.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DrawPyramid {
     public static void main(String[] args) {
         // Write a program that reads a number from the standard input, then draws a
@@ -10,6 +12,17 @@ public class DrawPyramid {
         // *******
         //
         // The pyramid should have as many lines as the number was
-        
-    }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many rows should your pyramid have?");
+        int numberOfLines = scanner.nextInt();
+
+        int rowNumber = 1;
+
+        int numberOfSpace = 0;
+        int numberOfStars = 1;
+
+        while (rowNumber < numberOfLines + 1) {
+            if ()System.out.print();
+        }
 }
