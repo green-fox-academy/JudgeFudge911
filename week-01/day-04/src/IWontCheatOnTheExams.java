14e7b5522a8f5f74b236fa57eae2b71c7608679c
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 17:20:18 +0200

    IWontCheatOnTheExams start

---
+++ week-01/day-04/src/IWontCheatOnTheExams.java
@@ -0,0 +1,7 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+        
+    }
+}
