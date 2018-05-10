ec7b0e8e36998a47b3918d20c7e80736bd3714d8
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 17:22:28 +0200

    IWontCheatOnTheExams completed

--- week-01/day-04/src/IWontCheatOnTheExams.java
+++ week-01/day-04/src/IWontCheatOnTheExams.java
@@ -1,7 +1,12 @@
 public class IWontCheatOnTheExams {
     public static void main(String[] args) {
-    // Create a program that writes this line 100 times:
-    // "I won't cheat on the exam!"
-        
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+        int numberOfLines = 0;
+
+        while (numberOfLines < 100) {
+            numberOfLines++;
+            System.out.println("I won't cheat on the exam!");
+        }
     }
 }
