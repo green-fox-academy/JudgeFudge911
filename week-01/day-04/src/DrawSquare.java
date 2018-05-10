ec0dfe10e36ee7c16d5d52e08ab47fde5ef27fe9
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-11 00:50:54 +0200

    Draw Square completed

--- week-01/day-04/src/DrawSquare.java
+++ week-01/day-04/src/DrawSquare.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DrawSquare {
     public static void main(String[] args) {
     // Write a program that reads a number from the standard input, then draws a
@@ -12,6 +14,27 @@ public class DrawSquare {
     // %%%%%
     //
     // The square should have as many lines as the number was
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of lines.");
+        int numberOfLines = scanner.nextInt();
+
+        if (numberOfLines <= 2 ) {
+            System.out.println("That's not enough lines.");
+        } else {
+            String fullRow = "# ";
+            for (int i = 1; i < numberOfLines ; i++) {
+                fullRow += "# ";
+            }
+            String row = "#";
+            for (int i = 2; i <numberOfLines ; i++) {
+                row += "  ";
+            }
+            row += " #";
+            System.out.println(fullRow);
+            for (int i = 2; i <numberOfLines ; i++) {
+                System.out.println(row);
+            }
+            System.out.println(fullRow);
+        }
     }
 }
