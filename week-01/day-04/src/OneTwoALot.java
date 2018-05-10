cf86aa2ebd96f6932d3aa908ef8d117300ecad89
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 16:50:28 +0200

    OneTwoALot Completed

--- week-01/day-04/src/OneTwoALot.java
+++ week-01/day-04/src/OneTwoALot.java
@@ -7,6 +7,20 @@ public class OneTwoALot {
     // If the number is one it should print: One
     // If the number is two it should print: Two
     // If the number is more than two it should print: A lot
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number.");
+
+        int number = scanner.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Not enough");
+        } else if (number == 1){
+            System.out.println("One");
+        } else if (number == 2) {
+            System.out.println("Two");
+        } else {
+            System.out.println("A lot");
+        }
     }
 }
