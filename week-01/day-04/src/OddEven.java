66528172a73c2d63f76b2f3a1bd94b781646dda0
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 16:42:36 +0200

    OddEven works

--- week-01/day-04/src/OddEven.java
+++ week-01/day-04/src/OddEven.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class OddEven {
     public static void main(String[] args) {
         // Write a program that reads a number from the standard input,
         // Then prints "Odd" if the number is odd, or "Even" if it is even.
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number!");
+
+        int number = scanner.nextInt();
+
+        String print = (number % 2 == 0) ? "This is an even number" : "This is an odd number";
+
+        System.out.println(print);
     }
 }
