d10d06f4a4e155d49ae13ce68924194285938b9d
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 13:39:55 +0200

    DefineBasicInfo complited

--- week-01/day-04/src/DefineBasicInfo.java
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -5,5 +5,15 @@ public class DefineBasicInfo {
         // Your age as an integer
         // Your height in meters as a double
         // Whether you are married or not as a boolean
+        String name = "Wermer Gábor";
+        int age = 24;
+        double heightMeters = 1.8;
+        boolean married = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(heightMeters);
+        System.out.println(married);
+
     }
 }
