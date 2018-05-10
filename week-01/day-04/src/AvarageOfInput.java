dad928582f07faa09653fb82c8394a2025f73122
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 16:28:54 +0200

    AvarageOfInput works

--- week-01/day-04/src/AvarageOfInput.java
+++
@@ -1,8 +0,0 @@
-public class AvarageOfInput {
-    public static void main(String[] args) {
-// Write a program that asks for 5 integers in a row,
-// then it should print the sum and the average of these numbers like:
-//
-// Sum: 22, Average: 4.4
-    }
-}
