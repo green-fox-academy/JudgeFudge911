fbc9c6a0b86fc9aecdb1c202bf048f4bcb6dfda6
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 14:17:34 +0200

    Cuboid complited

--- week-01/day-04/src/Cuboid.java
+++ week-01/day-04/src/Cuboid.java
@@ -1,9 +1,18 @@
 public class Cuboid {
-
+    public static void main(String[] args) {
 // Write a program that stores 3 sides of a cuboid as variables (doubles)
 // The program should write the surface area and volume of the cuboid like:
 //
 // Surface Area: 600
 // Volume: 1000
-    
+        double sideOne = 0;
+        double sideTwo = 0;
+        double sideThree = 0;
+
+        int surfaceArea = (int) (2 * sideOne * sideTwo + 2 * sideOne * sideThree + 2 * sideThree * sideTwo);
+        int volume = (int) (sideOne * sideTwo * sideThree);
+
+        System.out.println("Surface Area: " + surfaceArea);
+        System.out.println("Volume: " + volume);
+    }
 }
