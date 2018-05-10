5182aa94994b969a5c44e350623987b9c8bda4bf
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 13:31:24 +0200

    Swap complited

--- week-01/day-04/src/Swap.java
+++ week-01/day-04/src/Swap.java
@@ -3,7 +3,9 @@ public class Swap {
         // Swap the values of the variables
         int a = 123;
         int b = 526;
-
+        int temporarly = b;
+        b = a;
+        a = temporarly;
         System.out.println(a);
         System.out.println(b);
     }
