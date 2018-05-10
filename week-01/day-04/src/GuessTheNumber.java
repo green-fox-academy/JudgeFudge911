921db471d9bb50ca46e33bd954535f3fb7ea4eb8
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-11 01:09:02 +0200

    GuessTheNumber completed

--- week-01/day-04/src/GuessTheNumber.java
+++ week-01/day-04/src/GuessTheNumber.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GuessTheNumber {
     public static void main(String[] args) {
         // Write a program that stores a number, and the user has to figure it out.
@@ -7,6 +9,25 @@ public class GuessTheNumber {
         // The stored number is higher
         // The stried number is lower
         // You found the number: 8
+        Scanner scanner = new Scanner(System.in);
 
+        int storedNumber = 8;
+
+        System.out.println("Guess the number!");
+        int guess = scanner.nextInt();
+        boolean found = false;
+
+        while (found != true) {
+            if (storedNumber > guess) {
+                System.out.println("The stored number is higher, try again!");
+                guess = scanner.nextInt();
+            } else if (storedNumber < guess) {
+                System.out.println("The stored number is lower, try again!");
+                guess = scanner.nextInt();
+            } else {
+                System.out.println("You found the number: " + storedNumber);
+                found = true;
+            }
+        }
     }
 }
