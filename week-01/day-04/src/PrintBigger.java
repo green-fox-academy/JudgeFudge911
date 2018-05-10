9740c6b16c6f71efea1c2918f130a1d32fdfd3b4
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 16:57:18 +0200

    PrintBigger completed

--- week-01/day-04/src/PrintBigger.java
+++ week-01/day-04/src/PrintBigger.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class PrintBigger {
     public static void main(String[] args) {
         // Write a program that asks for two numbers and prints the bigger one
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter two numbers.");
+
+        int numberOne = scanner.nextInt();
+        int numberTwo = scanner.nextInt();
+
+        if (numberOne < numberTwo) {
+            System.out.println(numberTwo);
+        } else {
+            System.out.println(numberOne);
+        }
+
     }
 }
