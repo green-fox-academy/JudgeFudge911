e5170142c01866b1f23105ffb9f4b3feec29b368
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-10 17:26:30 +0200

    PrintEven completed

--- week-01/day-04/src/PrintEven.java
+++ week-01/day-04/src/PrintEven.java
@@ -1,6 +1,15 @@
 public class PrintEven {
     public static void main(String[] args) {
     // Create a program that prints all the even numbers between 0 and 500
-        
+        int firstNumber = 0;
+        int lastNumber = 500;
+
+
+        while (firstNumber < lastNumber+1) {
+            if (firstNumber % 2 == 0){
+                System.out.println(firstNumber);
+            }
+            firstNumber++;
+        }
     }
 }
