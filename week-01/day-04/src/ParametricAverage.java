4546040fc119563715b5b99ea8f0d80e9b7131a7
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-11 02:05:42 +0200

    ParametricAvarege get numbers done

--- week-01/day-04/src/ParametricAverage.java
+++ week-01/day-04/src/ParametricAverage.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ParametricAverage {
     public static void main(String[] args) {
         // Write a program that asks for a number.
@@ -6,6 +8,18 @@ public class ParametricAverage {
         // integers like:
         //
         // Sum: 22, Average: 4.4
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int thisMany = 3;
+
+        for (int i = 0; i < thisMany; i++) {
+            if (i != 0) {
+                System.out.println("Enter another number.");
+                int number = scanner.nextInt();
+            } else {
+                System.out.println("Enter a number.");
+                int number = scanner.nextInt();
+            }
+        }
     }
 }
