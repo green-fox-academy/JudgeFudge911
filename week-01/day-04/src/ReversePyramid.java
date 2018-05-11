51769a9d7c809b0dfbda80eec0b05fcb51647b93
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-11 12:14:50 +0200

    RevrsePyramid anti pattern needs reversing

--- week-01/day-04/src/ReversePyramid.java
+++ week-01/day-04/src/ReversePyramid.java
@@ -21,10 +21,28 @@ public class ReversePyramid {
         int numberOfColumn = numberOfRows + columnIncrement;
         int columnHalf = numberOfColumn/2;
         String pattern = "*";
+        String row = "";
+        String antiPattern = "0";
         for (int i = 3; i < numberOfColumn ; i++) {
             pattern += "**";
         }
-        System.out.println(pattern.length());
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j < numberOfColumn; j++) {
+                if (j == columnHalf-i){
+                    row += pattern;
+                    pattern = pattern.substring(0, pattern.length()-3);
+                } else if (j < columnHalf-i){
+                    row += antiPattern;
+                }
+            }
+            if (i == 0) {
+                row = row.substring(0, numberOfColumn+1);
+            } else {
+                row = row.substring(numberOfColumn+i, 2*numberOfColumn+2*i+1);
+            }
+            System.out.println(row);
+        }
+        System.out.println();
 
         }
 
