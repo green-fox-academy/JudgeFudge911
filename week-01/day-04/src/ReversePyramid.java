7cd4cbc89e7ef019872b98286027c8c181b9c90c
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-13 19:44:05 +0200

    reversePyramid commented

--- week-01/day-04/src/ReversePyramid.java
+++ week-01/day-04/src/ReversePyramid.java
@@ -2,48 +2,46 @@ import java.util.Scanner;
 
 public class ReversePyramid {
     public static void main(String[] args) {
-        // Write a program that reads a number from the standard input, then draws a
-        // pyramid like this:
-        //
-        //
-        //    *
-        //   ***
-        //  *****
-        // *******
-        //
-        // The pyramid should have as many lines as the number was
         Scanner scanner = new Scanner(System.in);
 
+        //GET NUMBER
         System.out.println("How many rows should your pyramid have?");
         int numberOfRows = scanner.nextInt();
-        int numberOfColumn = 2 * numberOfRows - 1;
 
-
+        //PATTERN
         String pattern = "*";
         String patternIncrement = "**";
         for (int i = 0; i < numberOfRows - 1; i++) {
             pattern += patternIncrement;
         }
+
+        //ANTI PATTERN
         String antiPattern = "";
         String antiPatternIncrement = "0";
 
         for (int i = 0; i < numberOfRows; i++) {
+
+            //RESET STRING AT EVERY ROW
             String row = "";
+
+            //FIRST ROW DOESN'T GET SPACE EVERY OTHER DOES INCREASINGLY MORE
             if (i != 0) {
                 antiPattern += antiPatternIncrement;
             }
+
+            //EVERY ROW DECREASINGLY LESS PATTERN
             row += antiPattern;
             row += pattern;
-            if (i != numberOfRows-1){
+            if (i != numberOfRows - 1) {
                 pattern = pattern.substring(0, pattern.length() - 2);
             }
-            for (int j = 0; j < numberOfColumn; j++) {
 
-            }
+            //PRINT OUT STRING ROW
             System.out.println(row);
         }
-
-
     }
 
 }
+
+
+
