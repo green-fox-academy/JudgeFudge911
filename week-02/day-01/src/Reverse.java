0ec2a9b2dbc733f632963c36993302f884b875f5
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-14 14:55:50 +0200

    Reverse done

---
+++ week-02/day-01/src/Reverse.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Reverse {
+  public static void main(String[] args) {
+    int[] aj = {3, 4, 5, 6, 7};
+    int temp = aj.length-1;
+    int[] ja = new int[5];
+
+    for (int num :
+            aj) {
+      ja[temp] = num;
+      temp--;
+    }
+    System.out.println(Arrays.toString(ja));
+  }
+}
