925646994e6ecabc40fc14a8cf5a962c3cb53d2a
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-14 17:17:57 +0200

    bubble done

---
+++ week-02/day-01/src/Bubble.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Bubble {
+  public static void main(String[] args) {
+    //  Create a function that takes a list of numbers as parameter
+    //  Returns a list where the elements are sorted in ascending numerical order
+    //  Make a second boolean parameter, if it's `True` sort that list descending
+
+    //  Example:
+    System.out.println(Arrays.toString(advancedBubble(new int[]{34, 12, 24, 9, 5}, false)));
+    //  should print [34, 24, 12, 9, 5]
+  }
+
+  public static int[] advancedBubble(int[] list, boolean order) {
+
+    Arrays.sort(list);
+
+    if (order == true) {
+      int temp = list.length - 1;
+      int[] reverseList = new int[list.length];
+      for (int num :
+              list) {
+        reverseList[temp] = num;
+        temp--;
+      }
+      return reverseList;
+    } else {
+      return list;
+    }
+  }
+}
--- week-02/day-01/src/Reverse.java
+++ week-02/day-01/src/Reverse.java
@@ -4,7 +4,7 @@ public class Reverse {
   public static void main(String[] args) {
     int[] aj = {3, 4, 5, 6, 7};
     int temp = aj.length-1;
-    int[] ja = new int[5];
+    int[] ja = new int[aj.length];
 
     for (int num :
             aj) {
