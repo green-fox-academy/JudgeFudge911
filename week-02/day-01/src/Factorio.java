7c74fca8a3ad62424e3408eab68a8117b3091d27
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-14 13:08:05 +0200

    factorio done

---
+++ week-02/day-01/src/Factorio.java
@@ -0,0 +1,20 @@
+public class Factorio {
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `factorio`
+//   that returns it's input's factorial
+
+  public static void main(String[] args) {
+    int number = 5;
+    System.out.println(factorio(number));
+  }
+  public static int factorio(int number){
+    int factorial = 1;
+    for (int i = 1; i < number + 1; i++) {
+      factorial *= i;
+    }
+    return factorial;
+  }
+
+}
