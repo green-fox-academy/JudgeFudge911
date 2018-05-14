835522197aa72d87a2c9f3c4475beede4638ef94
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-14 13:12:51 +0200

    comparelength done

---
+++ week-02/day-01/src/CompareLength.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class CompareLength {
+
+  public static void main(String[] args) {
+    int[] p1 = {1,2,3};
+
+    int[] p2 = {4,5};
+
+    if (p2.length > p1.length){
+      System.out.println(Arrays.toString(p2));
+    } else {
+      System.out.println(Arrays.toString(p1));
+    }
+  }
+}
