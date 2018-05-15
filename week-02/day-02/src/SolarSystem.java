6502c5527dda80b5caf10ace350db10f0fcaffc1
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-15 12:47:14 +0200

    SolarSystem

---
+++ week-02/day-02/src/SolarSystem.java
@@ -0,0 +1,19 @@
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SolarSystem {
+  public static void main(String[] args) {
+    ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+
+    System.out.println(putSaturn(planetList));
+  }
+  public static ArrayList putSaturn(ArrayList before){
+
+    before.add("Saturn");
+
+    return before;
+
+  }
+}
