89d3cdf876d40a26fcf7290cbe0e64fecef1d186
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-15 13:36:29 +0200

    AppendLetter done

---
+++ week-02/day-02/src/AppendLetter.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class AppendLetter {
+  public static void main(String[] args) {
+    List<String> far = Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk");
+
+    System.out.println(appendA(far));
+  }
+  public static List appendA(List name){
+
+
+    for (Object word :
+            name) {
+
+      name.set(name.indexOf(word), word + "a");
+    }
+    return name;
+
+  }
+}
