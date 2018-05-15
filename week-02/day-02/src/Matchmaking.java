98906841bd8b2e0b7b98efd9aa89aad7b1541176
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-15 13:29:31 +0200

    MAtchmaking will get back to this later

---
+++ week-02/day-02/src/Matchmaking.java
@@ -0,0 +1,20 @@
+/*import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Matchmaking {
+  public static void main(String[] args) {
+    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
+    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
+    System.out.println(makingMatches(girls, boys));
+
+  }
+
+  public static ArrayList makingMatches(ArrayList a, ArrayList b) {
+    ArrayList<String> c = new ArrayList<String>();
+
+
+    }
+
+    return c;
+  }
+}*/
