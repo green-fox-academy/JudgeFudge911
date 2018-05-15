b571ce4e8c8e96e33519b63e27553f83613c6fa1
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-15 11:36:21 +0200

    UrlFixer done

---
+++ week-02/day-02/src/UrlFixer.java
@@ -0,0 +1,9 @@
+public class UrlFixer {
+  public static void main(String[] args) {
+    String url = "https//www.reddit.com/r/nevertellmethebots";
+    url = url.replaceFirst("/", ":/");
+    url = url.replace("bots", "odds");
+
+    System.out.println(url);
+  }
+}
