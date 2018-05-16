e1b7f99e48903e5f3f1cb2975eab36b4529c75cd
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-16 16:03:28 +0200

    loop scarry

---
+++ week-02/day-03/src/Doubled.java
@@ -0,0 +1,36 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doubled {
+  public static void main(String[] args) {
+
+    decryptor("doubled.txt");
+  }
+
+  public static void decryptor(String file) {
+    Path path = Paths.get(file);
+
+    try {
+
+      List<String> lines = Files.readAllLines(path);
+
+      List<String> print = new ArrayList<>();
+
+      for (String s :
+              lines) {
+        for (int i = 0; i < s.length(); i =+ 2) {
+          print.add(s.substring(i,i+1));
+        }
+        System.out.println(print);
+
+
+      }
+
+    } catch (Exception e) {
+
+    }
+  }
+}
