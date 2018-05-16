5bc85cd71a8ad48103a9c66fc533b0374bc65d30
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-16 11:52:02 +0200

    DividedByZero done

--- week-02/day-03/src/DividedByZero.java
+++ week-02/day-03/src/DividedByZero.java
@@ -1,5 +1,16 @@
 public class DividedByZero {
   public static void main(String[] args) {
 
+    int number = 0;
+
+    divideTen(number);
+  }
+  public static void divideTen(int number) {
+    try {
+      System.out.println(10 / number);
+    } catch (ArithmeticException e) {
+      System.out.println("fail");
+    }
+
   }
 }
