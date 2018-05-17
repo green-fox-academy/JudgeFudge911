918883ba119962708075c3d5f545832bbd870d95
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-17 22:29:31 +0200

    LinePlay start

---
+++ week-02/day-05/src/LinePlay.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlay {
+  public static void mainDraw(Graphics graphics){
+
+
+  }
+
+
+  static int WIDTH = 400;
+  static int HEIGHT = 400;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT+23));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
