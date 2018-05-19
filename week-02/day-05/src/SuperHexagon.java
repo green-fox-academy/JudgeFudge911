0c2b9a56eb3382d193c8bab1cb4204a37d98a8dd
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-19 17:56:42 +0200

    Superhexagon other half oh colums need increasing number of hexagons

--- week-02/day-05/src/SuperHexagon.java
+++ week-02/day-05/src/SuperHexagon.java
@@ -7,7 +7,7 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class SuperHexagon {
   public static void mainDraw(Graphics graphics) {
 
-    int elemenntsPerSide = 3;
+    int elemenntsPerSide = 4;
 
     int elementsPerHeight = 2 * elemenntsPerSide - 1;
 
@@ -35,28 +35,47 @@ public class SuperHexagon {
 
     int[] xPoints2 = {WIDTH, WIDTH - hexagonWidth / 4, WIDTH - (int) (0.75 * hexagonWidth), WIDTH - hexagonWidth, WIDTH - (int) (0.75 * hexagonWidth), WIDTH - hexagonWidth / 4};
 
+    int[] xPoints3 = {hexagonWidth, hexagonWidth + hexagonWidth / 4, hexagonWidth + (int) (0.75 * hexagonWidth), 2*hexagonWidth, hexagonWidth + (int) (0.75 * hexagonWidth), hexagonWidth + hexagonWidth / 4};
+
+    int[] xPoints4 = {WIDTH-hexagonWidth, WIDTH - (int)(1.25*hexagonWidth), WIDTH - (int) (1.75 * hexagonWidth), WIDTH - 2*hexagonWidth, WIDTH - (int) (1.75 * hexagonWidth), WIDTH - (int)(1.25*hexagonWidth)};
+
     int[] yPoints1 = {zeroHeightPoint1 + hexagonHeight / 2, zeroHeightPoint1, zeroHeightPoint1, zeroHeightPoint1 + hexagonHeight / 2, zeroHeightPoint1 + hexagonHeight, zeroHeightPoint1 + hexagonHeight};
 
     int[] yPoints2 = {zeroHeightPoint2 - hexagonHeight / 2, zeroHeightPoint2, zeroHeightPoint2, zeroHeightPoint2 - hexagonHeight / 2, zeroHeightPoint2 - hexagonHeight, zeroHeightPoint2 - hexagonHeight};
 
+    int[] yPoints3 = {zeroHeightPoint1, zeroHeightPoint1 - hexagonHeight/2, zeroHeightPoint1 - hexagonHeight/2, zeroHeightPoint1, zeroHeightPoint1 + hexagonHeight/2, zeroHeightPoint1 + hexagonHeight/2};
 
-    for (int i = 2; i < 4; i++) {
+    int[] yPoints4 = {zeroHeightPoint2, zeroHeightPoint2+hexagonHeight/2, zeroHeightPoint2+hexagonHeight/2, zeroHeightPoint2 , zeroHeightPoint2 - hexagonHeight/2, zeroHeightPoint2 - hexagonHeight/2};
+
+    for (int i = 0; i < 2; i++) {
       for (int j = 0; j < elemenntsPerSide; j++) {
         graphics.setColor(new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256)));
         graphics.fillPolygon(xPoints1, yPoints1, 6);
-        /*graphics.fillPolygon(xPoints2, yPoints2, 6)*/;
+
+        graphics.fillPolygon(xPoints2, yPoints2, 6);
+
+        graphics.fillPolygon(xPoints3, yPoints3, 6);
+
+        graphics.fillPolygon(xPoints4, yPoints4, 6);
+
         for (int k = 0; k < 6; k++) {
           yPoints1[k] += hexagonHeight;
           yPoints2[k] -= hexagonHeight;
+          yPoints3[k] += hexagonHeight;
+          yPoints4[k] -= hexagonHeight;
         }
       }
       for (int k = 0; k < 6; k++) {
         yPoints1[k] -= (elemenntsPerSide+1)*hexagonHeight;
         yPoints2[k] += (elemenntsPerSide+1)*hexagonHeight;
+        yPoints3[k] -= (elemenntsPerSide+1)*hexagonHeight;
+        yPoints4[k] += (elemenntsPerSide+1)*hexagonHeight;
       }
       for (int k = 0; k < 6; k++) {
         xPoints1[k] += hexagonWidth*2;
         xPoints2[k] -= hexagonWidth*2;
+        xPoints3[k] += hexagonWidth*2;
+        xPoints4[k] -= hexagonWidth*2;
       }
       elemenntsPerSide += 2;
 
