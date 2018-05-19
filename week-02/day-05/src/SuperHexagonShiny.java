e7a4e35f1dd961682ff8a0d7e272b139e4ad591c
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-19 23:30:10 +0200

    SuperHexagonShiny: Graphics canvas is now static

--- week-02/day-05/src/SuperHexagonShiny.java
+++ week-02/day-05/src/SuperHexagonShiny.java
@@ -5,7 +5,16 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class SuperHexagonShiny {
-  public static void mainDraw(Graphics graphics) {
+
+  public static final int WIDTH = 400;
+  public  static final int HEIGHT = 400;
+
+  private static Graphics canvas;
+
+  public static void mainDraw() {
+
+
+
 
     int elementsPerSide = 4;
 
@@ -15,12 +24,12 @@ public class SuperHexagonShiny {
 
     int hexagonHeight = 0;
 
-    drawHexagon(graphics,100,100,true);
+    drawHexagon(100,100,true);
 
 
   }
 
-  public static void drawHexagon(Graphics graphics, int xZero, int yZero, boolean evenColumn) {
+  public static void drawHexagon(int xZero, int yZero, boolean evenColumn) {
 
     int hexagonWidth = 20;
 
@@ -35,15 +44,21 @@ public class SuperHexagonShiny {
 
       int nPoints = 6;
 
-      graphics.drawPolygon(x, y, nPoints);
+      canvas.drawPolygon(x, y, nPoints);
+
     } else {
+
+      int[] x = {xZero, xZero + hexagonWidth / 4, xZero + (int) (0.75 * hexagonWidth), xZero + hexagonWidth, xZero + (int) (0.75 * hexagonWidth), xZero + hexagonWidth / 4};
+
+      int[] y = {yZero, yZero - hexagonHeight / 2, yZero - hexagonHeight / 2, yZero, yZero + hexagonHeight / 2, yZero + hexagonHeight / 2};
+
+      int nPoints = 6;
+
+      canvas.drawPolygon(x, y, nPoints);
 
     }
 
   }
-
-  static int WIDTH = 400;
-  static int HEIGHT = 400;
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
@@ -58,7 +73,8 @@ public class SuperHexagonShiny {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
-      mainDraw(graphics);
+      canvas = graphics;
+      mainDraw();
     }
   }
 }
