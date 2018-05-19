9da55059ac9715175297885673beaf9b9b2dbc10
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-19 11:52:53 +0200

    SuperHexagon one of vertical lines done?

--- week-02/day-05/src/SuperHexagon.java
+++ week-02/day-05/src/SuperHexagon.java
@@ -9,16 +9,27 @@ public class SuperHexagon {
 
     int elemenntsPerSide = 4;
 
-    int elementsPerHeight = 2*elemenntsPerSide -1;
+    int elementsPerHeight = 2 * elemenntsPerSide - 1;
 
-    int hexagonHeight = HEIGHT/elementsPerHeight;
+    int hexagonHeight = HEIGHT / elementsPerHeight;
 
-    int hexagonSide = (int)(hexagonHeight/Math.sqrt(3));
+    double temp = (hexagonHeight/ Math.sqrt(3) - (int)(hexagonHeight/ Math.sqrt(3)));
+
+    int hexagonSide = 0;
+
+    if (temp > 0.5){
+      hexagonSide = (int) (hexagonHeight/ Math.sqrt(3)) + 1;
+    } else {
+      hexagonSide = (int) (hexagonHeight/ Math.sqrt(3));
+    }
 
     int hexagonWidth = hexagonSide * 2;
 
-    for (int i = 1; i < HEIGHT/hexagonHeight; i++) {
-      graphics.drawLine(WIDTH/2-i*hexagonSide, i*hexagonHeight/2, WIDTH/2+ i * hexagonSide, i*hexagonHeight/2);
+    for (int i = 1; i < HEIGHT / hexagonHeight+1; i++) {
+      graphics.drawLine(WIDTH / 2 - i * hexagonSide, i * hexagonHeight / 2, WIDTH / 2 + i * hexagonSide, i * hexagonHeight / 2);
+
+      graphics.drawLine(WIDTH - i * 2 * hexagonSide, HEIGHT / 2, WIDTH - hexagonSide * i, HEIGHT / 2 - hexagonHeight * i);
+
     }
 
 
