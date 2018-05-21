0b9ff3a6d6bf248fff205e378f4cf5bc90c4917b
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-21 09:51:11 +0200

    DrawDiamond: works with odd numberOfRows

--- week-01/day-04/src/T_30_DrawDiamond.java
+++ week-01/day-04/src/T_30_DrawDiamond.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class T_30_DrawDiamond {
@@ -6,8 +7,22 @@ public class T_30_DrawDiamond {
     System.out.println("How many rows should your diamond have?");
     int numberOfRows = scanner.nextInt();
 
+    int patternCounter = numberOfRows/2;
 
-
-
+    for (int i = 0; i < numberOfRows; i++) {
+      for (int j = 0; j < numberOfRows; j++) {
+        if (j < patternCounter || j > numberOfRows-1-patternCounter) {
+          System.out.print("0");
+        } else {
+          System.out.print("*");
+        }
+      }
+      System.out.println();
+      if (i < numberOfRows/2){
+        patternCounter--;
+      } else {
+        patternCounter++;
+      }
+    }
   }
 }
