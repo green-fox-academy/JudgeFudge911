dc08a1b9967196f04cab2fd6cdff68a40ec283fc
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-23 14:59:28 +0200

    Teacher Student done

---
+++ week-03/day-03/TeachersAndStudents/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+  public static void learn(){
+
+  }
+
+  public static void question(Teacher teacher){
+    teacher.answer();
+  }
+
+}
---
+++ week-03/day-03/TeachersAndStudents/src/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+
+  public static void  answer(){
+
+  }
+
+  public static void teach(Student student){
+    student.learn();
+  }
+
+}
