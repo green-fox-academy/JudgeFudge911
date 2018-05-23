16336a8470c3f5a333017ddc194f2b4b37fdb8f1
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-23 12:56:01 +0200

    PostIt done

---
+++ week-03/day-03/PostIt/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    PostIt obj = new PostIt("orange", "Idea 1", "blue");
+    PostIt obj1 = new PostIt("pink", "Awesome", "black");
+    PostIt obj2 = new PostIt("yellow", "Suberb!", "greens");
+  }
+}
---
+++ week-03/day-03/PostIt/src/PostIt.java
@@ -0,0 +1,11 @@
+public class PostIt {
+  String background;
+  String text;
+  String textColor;
+
+  PostIt(String background, String text, String textColor) {
+    this.background = background;
+    this.text = text;
+    this.textColor = textColor;
+  }
+}
