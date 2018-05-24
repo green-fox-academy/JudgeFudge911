a9bc4cc9ef741202967bb5fa68b069477a743246
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-25 00:28:39 +0200

    Lottery(feat): readNumbers, findMostCommon, printOutMostCommon add

--- week-02/day-03/src/T_13_Lottery.java
+++ week-02/day-03/src/T_13_Lottery.java
@@ -8,39 +8,68 @@ public class T_13_Lottery {
   public static void main(String[] args) {
     // Create a method that find the 5 most common lottery numbers in lottery.csv
 
-    HashMap<Integer, Integer> lotteryNumbers = new HashMap<>();
-    try {
-      Scanner isItOver = new Scanner(new File("lottery.txt"));
+    printOutMostCommon(findMostCommon(readNumbers("lottery.txt")));
 
+    //OR
+
+    /*HashMap<Integer, Integer> lotteryNumbers = readNumbers("lottery.txt");
+
+    int[] mostCommonNumbers = findMostCommon(lotteryNumbers);
+
+    printOutMostCommon(mostCommonNumbers);*/
+
+  }
+
+  public static HashMap<Integer, Integer> readNumbers(String fileName) {
+    try {
+      Scanner fileScanner = new Scanner(new File(fileName));
       String[] currentLine;
       Integer currentNumber;
 
-      while (isItOver.hasNext()) {
-        currentLine = isItOver.nextLine().split(";");
+      HashMap<Integer, Integer> lotteryNumbers = new HashMap<>();
+
+      while (fileScanner.hasNext()) {
+        currentLine = fileScanner.nextLine().split(";");
         for (int i = 0; i < 5; i++) {
           currentNumber = Integer.parseInt(currentLine[currentLine.length - i - 1]);
           lotteryNumbers.merge(currentNumber, 1, Integer::sum);
         }
       }
+      return lotteryNumbers;
+    } catch (IOException e) {
+      System.out.println("Couldn't read file");
+      return null;
+    }
+  }
 
-      int[] mostCommonNumbers = new int[5];
-      for (int i = 0; i < 5; i++) {
-        Map.Entry<Integer, Integer> maxEntry = null;
-        for (Map.Entry<Integer, Integer> entry : lotteryNumbers.entrySet()) {
-          if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
-            maxEntry = entry;
-          }
+  public static int[] findMostCommon(HashMap<Integer, Integer> list) {
+
+    int numberOfElements = 5;
+
+    int[] mostCommonNumbers = new int[numberOfElements];
+
+    for (int i = 0; i < 5; i++) {
+      Map.Entry<Integer, Integer> maxEntry = null;
+      for (Map.Entry<Integer, Integer> entry : list.entrySet()) {
+        if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+          maxEntry = entry;
         }
-        mostCommonNumbers[i] = maxEntry.getKey();
-        lotteryNumbers.remove(maxEntry.getKey(), maxEntry.getValue());
       }
-      for (int number : mostCommonNumbers) {
-        System.out.print(number + ", ");
+      mostCommonNumbers[i] = maxEntry.getKey();
+      list.remove(maxEntry.getKey(), maxEntry.getValue());
+    }
+
+    return mostCommonNumbers;
+  }
+
+  public static void printOutMostCommon(int[] array) {
+    System.out.print("These are the 5 most common lottery numbers: ");
+    for (int i = 0; i < array.length; i++) {
+      if (i == array.length - 1) {
+        System.out.print(array[i] + ".");
+      } else {
+        System.out.print(array[i] + ", ");
       }
-    } catch (
-            IOException e)
-    {
-      System.out.println("Oh no");
     }
   }
 }
