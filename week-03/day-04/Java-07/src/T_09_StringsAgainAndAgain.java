c61024f8c2ae1b29124a200dcf706b3e73bcb825
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-24 12:39:36 +0200

    StringsAgainAndAgain done

---
+++ week-03/day-04/Java-07/src/T_09_StringsAgainAndAgain.java
@@ -0,0 +1,20 @@
+public class T_09_StringsAgainAndAgain {
+  public static void main(String[] args) {
+    // Given a string, compute recursively a new string where all the
+    // adjacent chars are now separated by a "*".
+
+    System.out.println(separate("abcdefg"));
+  }
+
+  public static String separate(String text){
+
+   if(text.length() == 1){
+     return text;
+   } else {
+     String call = text.substring(1, text.length());
+     return text.substring(0,1) + "*" + separate(call);
+   }
+
+  }
+
+}
