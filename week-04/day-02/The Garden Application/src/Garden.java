580f34c3eec9fc60975558d520e61f31b37369b3
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-29 12:22:39 +0200

    The Garden Application structure

---
+++ week-04/day-02/The Garden Application/src/Garden.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Garden {
+
+  List<Flower> flowers;
+  List<Tree> trees;
+
+  public void watering(){
+
+
+  }
+
+}
---
+++ week-04/day-02/The Garden Application/src/Plants.java
@@ -0,0 +1,12 @@
+abstract public class Plants {
+
+  private int water;
+
+  abstract boolean needWater();
+
+  abstract boolean checkWater();
+
+  abstract void setWater();
+
+  abstract int getWater();
+}
