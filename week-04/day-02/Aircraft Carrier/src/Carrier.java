e7d9c269ac40f08251514aa84eb3aec268da62f1
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-29 13:27:09 +0200

    Aircrafts carrier healthpoints var and contructor, add function

--- week-04/day-02/Aircraft Carrier/src/Carrier.java
+++ week-04/day-02/Aircraft Carrier/src/Carrier.java
@@ -5,9 +5,15 @@ public class Carrier {
 
   List<Aircraft> aircrafts;
   int storeOfAmmo;
+  int healthPoints;
 
-  public Carrier(int storeOfAmmo) {
+  public Carrier(int storeOfAmmo, int healthPoints) {
     aircrafts = new ArrayList<>();
     this.storeOfAmmo = storeOfAmmo;
+    this.healthPoints = healthPoints;
+  }
+
+  public void add(Aircraft aircraft){
+    aircrafts.add(aircraft);
   }
 }
