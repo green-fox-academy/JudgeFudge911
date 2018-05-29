d5a7c2655bf796d162e8e73fde491e18bb2e181f
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-29 12:54:31 +0200

    TheGardenApp: checkStatus function type and watering variables add

--- week-04/day-02/The Garden Application/src/Garden.java
+++ week-04/day-02/The Garden Application/src/Garden.java
@@ -14,7 +14,37 @@ public class Garden {
     trees.add(tree);
   }
 
+  public void checkStatus(){
 
+    List<Plants> plants = new ArrayList<>();
+    plants.addAll(flowers);
+    plants.addAll(trees);
+    String type = "";
+    String watering = "";
+
+    for (Plants plant :
+            plants) {
+      if (plant instanceof Flower){
+        type = "Flower";
+      } else {
+        type = "Tree";
+      }
+      if(plant.needWater()){
+        watering = " needs water";
+      } else {
+        watering = " doesn't need water";
+      }
+      System.out.println("The " + plant.color + " " + type + watering );
+    }
+
+  }
+
+  public List<Plants> listPlants(){
+    List<Plants> plants = new ArrayList<>();
+    plants.addAll(flowers);
+    plants.addAll(trees);
+    return plants;
+  }
 
   public void watering(int amountOfWatering) {
 
