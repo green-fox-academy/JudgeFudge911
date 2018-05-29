02dc80ea6eab8d37001a61f85e759a6ac3cda02e
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-29 13:09:13 +0200

    Aircraft structure

---
+++ week-04/day-02/Aircraft Carrier/src/Aircraft.java
@@ -0,0 +1,15 @@
+public class Aircraft {
+
+  int ammoStore;
+  int maxAmmo;
+  int baseDamage;
+
+  public Aircraft(){
+    ammoStore = 0;
+  }
+
+  public int fight(){
+
+  }
+
+}
---
+++ week-04/day-02/Aircraft Carrier/src/F16.java
@@ -0,0 +1,5 @@
+public class F16 extends Aircraft {
+
+
+
+}
