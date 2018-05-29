78d44a2636e7a62ace8ce9552ee53301de228828
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-29 12:31:51 +0200

    TheGardenApplication: Tree overrides Plants methods

--- week-04/day-02/The Garden Application/src/Tree.java
+++ week-04/day-02/The Garden Application/src/Tree.java
@@ -1,2 +1,13 @@
 public class Tree extends Plants {
+
+  @Override
+  boolean needWater() {
+    return (water < 10);
+  }
+
+  @Override
+  void getWatered(int amountOfWater) {
+    water += Math.round(amountOfWater * 0.4);
+  }
+
 }
