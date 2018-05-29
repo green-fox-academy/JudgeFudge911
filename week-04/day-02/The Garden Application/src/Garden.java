7f926af77fdb3336be6e291da341ef67376d0c2e
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-29 12:35:44 +0200

    TheGardenApplication: Garden watering function

--- week-04/day-02/The Garden Application/src/Garden.java
+++ week-04/day-02/The Garden Application/src/Garden.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Garden {
@@ -5,9 +6,26 @@ public class Garden {
   List<Flower> flowers;
   List<Tree> trees;
 
-  public void watering(){
+  public void watering(int amountOfWatering) {
 
+    List<Plants> plants = new ArrayList<>();
+    plants.addAll(flowers);
+    plants.addAll(trees);
+    int counter = 0;
+    for (Plants plant :
+            plants) {
+      if (plant.needWater()) {
+        counter++;
+      }
+    }
 
+    amountOfWatering /= counter;
+
+    for (Plants plant :
+            plants) {
+      if (plant.needWater()) {
+        plant.getWatered(amountOfWatering);
+      }
+    }
   }
-
 }
