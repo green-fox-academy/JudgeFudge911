99e44c17ab4d93e31c498f42c521da01d7ae3cf9
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-29 12:48:34 +0200

    TheGardenApp: App class with main method add, garden, flowers and trees initialized

--- week-04/day-02/The Garden Application/src/App.java
+++ week-04/day-02/The Garden Application/src/App.java
@@ -1,5 +1,15 @@
 public class App {
   public static void main(String[] args) {
 
+    Garden garden = new Garden();
+
+    Flower yellow = new Flower("yellow");
+    Flower blue = new Flower("blue");
+
+    Tree purple = new Tree("purple");
+    Tree orange = new Tree("orange");
+
+
+
   }
 }
--- week-04/day-02/The Garden Application/src/Garden.java
+++ week-04/day-02/The Garden Application/src/Garden.java
@@ -6,6 +6,16 @@ public class Garden {
   List<Flower> flowers;
   List<Tree> trees;
 
+  public void addFlower(Flower flower){
+    flowers.add(flower);
+  }
+
+  public void addTree(Tree tree){
+    trees.add(tree);
+  }
+
+
+
   public void watering(int amountOfWatering) {
 
     List<Plants> plants = new ArrayList<>();
