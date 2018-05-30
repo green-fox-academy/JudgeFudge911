16ce9d0504073ff8edde093d5f135300e6e2bf08
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-30 13:27:03 +0200

    Zoo: Animal -> eat() gender legs

--- week-04/day-03/Animals in the Zoo/src/Animal.java
+++ week-04/day-03/Animals in the Zoo/src/Animal.java
@@ -2,10 +2,16 @@ public abstract class Animal {
 
   private String name;
   int age;
+  String gender;
+  int legs;
 
 
   public String getName() {
     return name;
+  }
+
+  public void eat(){
+
   }
 
 }
