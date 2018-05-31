f56cccdd8fe409ab9745d795213b6c7131606977
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-31 12:58:06 +0200

    test(Sum): Sum returns zero when emptyListInput

--- week-04/day-04/Sum/src/Sum.java
+++ week-04/day-04/Sum/src/Sum.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 public class Sum {
 
   public int sumOfElements(ArrayList<Integer> numbers) {
-
+    if (numbers == null) {
+      return 0;
+    }
     int sum = 0;
     for (Integer number : numbers) {
       sum += number;
--- week-04/day-04/Sum/test/SumTest.java
+++ week-04/day-04/Sum/test/SumTest.java
@@ -1,3 +1,5 @@
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -6,17 +8,27 @@ import static org.junit.Assert.*;
 
 public class SumTest {
 
+  Sum sum = new Sum();
+
   @Test
   public void sumOfElements() {
-
-    Sum sum = new Sum();
-
     ArrayList<Integer> numbers = new ArrayList<>();
+
     for (int i = 0; i < 3; i++) {
       numbers.add(i);
     }
 
     int result = 3;
+
+    assertEquals(result, sum.sumOfElements(numbers));
+
+  }
+
+  @Test
+  public void should_return0_when_emptyListInput() {
+    ArrayList<Integer> numbers = new ArrayList<>();
+
+    int result = 0;
 
     assertEquals(result, sum.sumOfElements(numbers));
 
