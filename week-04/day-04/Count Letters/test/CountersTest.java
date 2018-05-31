a91f79eb9554bbe957f33a94431f3ad73ea04a70
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-31 13:29:26 +0200

    test(Counters): doesn't count blanks

--- week-04/day-04/Count Letters/test/CountersTest.java
+++ week-04/day-04/Count Letters/test/CountersTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class CountersTest {
 
   @Test
-  public void should_countLetters_whenSentenceInput() {
+  public void should_returnHashmapWithLettersAsKeysAndRepetitionsAsIntegers_whenSentenceInput() {
 
     Counters counters = new Counters();
 
@@ -13,6 +13,18 @@ public class CountersTest {
 
     Integer result = 4;
     assertEquals(result, counters.countLetters(text).get("e"));
+
+  }
+
+  @Test
+  public void should_returnNull_whenBlankInput() {
+
+    Counters counters = new Counters();
+
+    String text = "Well hello there";
+
+    Integer result = null;
+    assertEquals(result, counters.countLetters(text).get(" "));
 
   }
 }
