5da67a8a18fd34c6e0c12e1dfc4952f9ed698374
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-31 14:20:22 +0200

    feat(Extension): maxOfThree() now works for every parameter not just a and c

--- week-04/day-04/ExtensionTest/src/Extension.java
+++ week-04/day-04/ExtensionTest/src/Extension.java
@@ -7,9 +7,11 @@ public class Extension {
   }
 
   int maxOfThree(int a, int b, int c) {
-    if (a > b)
+    if (a > b && a > c) {
       return a;
-    else
+    } else if (b > a && b > c){
+      return b;
+    } else
       return c;
   }
 
