bccfafbf2815fc53163293ba5d55479bba279900
Wermer Gábor <wermer.gabor@gmail.com>
2018-05-31 16:18:08 +0200

    feat(CAB): CAB init

---
+++ week-04/day-04/CowsAndBulls/src/CAB.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class CAB {
+
+  Integer[] number;
+  boolean gameOver;
+  int guessCounter;
+
+
+  public String guess(Integer numbers) {
+    guessCounter++;
+    String status = "";
+    for (int i = 0; i < 4; i++) {
+      if (numbers.toString().charAt(i) == number[i]) {
+        status += "Cow";
+      } else if (Arrays.asList(number).contains(toString().charAt(i))) {
+        status += "Bull";
+      }
+    }
+    return status;
+  }
+}
+
