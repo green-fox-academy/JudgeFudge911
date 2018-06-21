60d814a53b2837f71d0248950317ef63bd8818b1
Wermer Gábor <wermer.gabor@gmail.com>
2018-06-21 23:47:00 +0200

    main content html

--- week-06/photogallery/main.js
+++ week-06/photogallery/main.js
@@ -3,12 +3,20 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 
+
+let photos = [
+    {id: 1, path: '/assets/imges/1.png', header: "I am sorry little one", description: "lorem ipsum", isActive: true},
+    {id: 2, path: '/assets/imges/2.png', header: "I am sorry little two", description: "lorem ipsum", isActive: false},
+    {id: 3, path: '/assets/imges/3.png', header: "I am sorry little three", description: "lorem ipsum", isActive: false}
+
+];
+
 app.set('view engine', 'ejs');
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
-    res.render('gallery');
+    res.render('gallery', {photos, __dirname});
 });
 
 app.listen(PORT, () => {
