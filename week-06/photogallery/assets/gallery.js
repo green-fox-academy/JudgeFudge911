792fc9537a3c4f2df2cfa8bd780f4c5a801aec40
Wermer Gábor <wermer.gabor@gmail.com>
2018-06-22 11:45:59 +0200

    thumbnail clicks deactivate current button

--- week-06/photogallery/assets/gallery.js
+++ week-06/photogallery/assets/gallery.js
@@ -1,26 +1,33 @@
 const activePicture = document.querySelector('#activePicture');
+const activeH1 = document.querySelector('#activeH1');
+const activeParagraph = document.querySelector('#activeP');
 const thumbnails = document.querySelector('.thumbnails');
 const thumbnailButton = document.querySelectorAll('.thumbnailButton');
 const leftButton = document.querySelector('#leftButton');
 const rightButton = document.querySelector('#rightButton');
+const picMaxIndex = thumbnailButton.length-1;
 
 thumbnails.addEventListener('click', changePicture, false);
 leftButton.addEventListener('click', rotateLeft, false);
 rightButton.addEventListener('click', rotateRight, false);
 
-function changePicture(e, currPic) {
+function changePicture(e, currPic, currIndex) {
     if (e.classList) {
         activePicture.src = e.firstChild.nextSibling.src;
+        /*activeH1.innerHTML = photos[currIndex].header;
+        activeParagraph.innerHTML = photos[currIndex].description;*/
         currPic.classList.remove('active');
         e.classList.add('active');
         e.focus = true;
     } else {
         if (e.target.src) {
             activePicture.src = e.target.src;
+            thumbnailButton[findActiveThumbnailIndex()].classList.remove('active');
             e.target.parentNode.classList.add('active');
             e.target.parentNode.focus = true;
         } else {
             activePicture.src = e.target.firstChild.nextSibling.src;
+            thumbnailButton[findActiveThumbnailIndex()].classList.remove('active');
             e.target.classList.add('active');
             e.target.focus = true;
         }
@@ -28,21 +35,24 @@ function changePicture(e, currPic) {
 }
 
 function rotateLeft() {
-    let buttons = Array.from(thumbnailButton);
-    let currIndex = 0;
-    buttons.forEach(e => {
-        if (e.classList.contains('active')) {
-            currIndex = buttons.indexOf(e);
-        }
-    });
+    let currIndex = findActiveThumbnailIndex();
     if (currIndex - 1 < 0) {
-        changePicture(thumbnailButton[buttons.length - 1], thumbnailButton[0])
+        changePicture(thumbnailButton[picMaxIndex], thumbnailButton[0],currIndex)
     } else {
-        changePicture(thumbnailButton[currIndex - 1], thumbnailButton[currIndex]);
+        changePicture(thumbnailButton[currIndex - 1], thumbnailButton[currIndex],currIndex);
     }
 }
 
 function rotateRight() {
+    let currIndex = findActiveThumbnailIndex();
+    if (currIndex + 1 > picMaxIndex) {
+        changePicture(thumbnailButton[0], thumbnailButton[picMaxIndex],currIndex)
+    } else {
+        changePicture(thumbnailButton[currIndex + 1], thumbnailButton[currIndex], currIndex);
+    }
+}
+
+function findActiveThumbnailIndex () {
     let buttons = Array.from(thumbnailButton);
     let currIndex = 0;
     buttons.forEach(e => {
@@ -50,10 +60,5 @@ function rotateRight() {
             currIndex = buttons.indexOf(e);
         }
     });
-    if (currIndex + 1 > buttons.length - 1) {
-        changePicture(thumbnailButton[0], thumbnailButton[buttons.length-1])
-    } else {
-        changePicture(thumbnailButton[currIndex + 1], thumbnailButton[currIndex]);
-    }
+    return currIndex;
 }
-
--- week-06/photogallery/main.js
+++ week-06/photogallery/main.js
@@ -4,9 +4,9 @@ const path = require('path');
 const PORT = 3000;
 
 let photos = [
-    {id: 1, path: '/assets/imges/1.png', header: "I am sorry little one", description: "lorem ipsum", isActive: true},
-    {id: 2, path: '/assets/imges/2.png', header: "I am sorry little two", description: "lorem ipsum", isActive: false},
-    {id: 3, path: '/assets/imges/3.png', header: "I am sorry little three", description: "lorem ipsum", isActive: false}
+    {path: '/assets/imges/1.png', header: "I am sorry little one", description: "lorem ipsum", isActive: 'active'},
+    {path: '/assets/imges/2.png', header: "I am sorry little two", description: "lorem ipsum", isActive: ''},
+    {path: '/assets/imges/3.png', header: "I am sorry little three", description: "lorem ipsum", isActive: ''}
 
 ];
 
