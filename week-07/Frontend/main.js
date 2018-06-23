8f9951da36edd862c20335be8db79121fdc3cd6b
Wermer Gábor <wermer.gabor@gmail.com>
2018-06-23 14:28:05 +0200

    doubling done

--- week-07/Frontend/main.js
+++ week-07/Frontend/main.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;
+const PORT = 8080;
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
-    res.sendFile('/home/gabor/JudgeFudge911.github.io/week-07/Frontend/assets/index.html')
+    res.sendFile(path.join(__dirname, 'assets/index.html'))
+});
+
+/*app.get('/doubling', (req, res) => {
+        res.send(JSON.stringify({error: "Please provide an input!"}));
+});*/
+
+app.get('/doubling?', (req, res) => {
+    res.send(JSON.stringify((req.query.input) ?
+        {recieved: Number(req.query.input), result: Number(req.query.input) * 2} :
+        {error: "Please provide an input!"}))
 });
 
 app.listen(PORT, () => {
