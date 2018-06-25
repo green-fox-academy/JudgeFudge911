18cd0ba672269f4c6d20a4dea068946f1c4896ad
Wermer Gábor <wermer.gabor@gmail.com>
2018-06-25 10:09:02 +0200

    i am groot test with input

--- week-07/REST-testing/iamgroot/test/test-routes.js
+++ week-07/REST-testing/iamgroot/test/test-routes.js
@@ -4,14 +4,26 @@ const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
 
-test('groot endpoint', (t) => {
+test('groot endpoint without input', (t) => {
     request(app)
         .get('/groot')
         .expect('Content-Type', /object/)
         .expect(200)
         .end(function(err, res) {
-            let expected = {error: "I am Groot!"}
-            t.same(res.body, expected, 'Error message as expected')
+            let expected = {error: "I am Groot!"};
+            t.same(res.body, expected, 'Error message as expected');
+            t.end();
+        })
+});
+
+test('groot endpoint with input', (t) => {
+    request(app)
+        .get('/groot?message=hello')
+        .expect('Content-Type', /object/)
+        .expect(200)
+        .end(function(err, res) {
+            let expected = {recieved: "hello", translated: "I am Groot!"};
+            t.same(res.body, expected, 'Translated as expected');
             t.end();
         })
 });
