63bfcacd612335dffa96f97010f389771eeae63d
Wermer Gábor <wermer.gabor@gmail.com>
2018-06-25 21:19:53 +0200

    giphy api done

--- week-07/XHR/giphyapi/main.js
+++ week-07/XHR/giphyapi/main.js
@@ -1,15 +1,25 @@
 const ourRequest = new XMLHttpRequest();
 const container = document.querySelector('.container');
+let data;
 ourRequest.open('GET', "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=Zc6j1Mm6HCTboLgfb1mFVeD7GMkYhb10&limit=16");
 ourRequest.onload = () => {
     let ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData);
+    data = ourData;
     for (let i = 0; i < 15; i++) {
-        console.log(ourData.data[i].url);
-        let img = document.createElement('img')
+        let img = document.createElement('img');
         img.src = ourData.data[i].images.original_still.url;
         container.appendChild(img);
+        img.addEventListener('click', playGiphy, false)
     }
 
 };
+
 ourRequest.send();
+
+function playGiphy(e) {
+    data.data.forEach(element => {
+        if (element.images.original_still.url === e.srcElement.currentSrc) {
+            e.target.src = element.images.original.url
+        }
+    })
+}
