bdd6d741b5aca19e6f7d15563373b1c595521a16
Wermer Gábor <wermer.gabor@gmail.com>
2018-06-25 12:48:25 +0200

    tests refactor

--- week-07/REST/test/main.js
+++ week-07/REST/test/main.js
@@ -12,11 +12,8 @@ test('Doubling without input', (t) => {
         .end((err, res) => {
             let expected = {error: "Please provide an input!"};
             t.same(res.body, expected, "Return error object as expected");
-            t.end();
         });
-});
 
-test('Doubling with input 2', (t) => {
     request(app)
         .get('/doubling?input=2')
         .expect('Content-Type', /object/)
@@ -24,11 +21,7 @@ test('Doubling with input 2', (t) => {
         .end((err, res) => {
             let expected = {recieved: 2, result: 4};
             t.same(res.body, expected, "Doubles input as expected");
-            t.end();
         });
-});
-
-test('Doubling with not string input', (t) => {
     request(app)
         .get('/doubling?input="hello"')
         .expect('Content-Type', /object/)
@@ -48,11 +41,7 @@ test('Greeter with no name', (t) => {
         .end((err, res) => {
             let expected = {error: "Please provide a name!"};
             t.same(res.body, expected, "Provide name error message correct");
-            t.end();
         });
-});
-
-test('Greeter with no title', (t) => {
     request(app)
         .get('/greeter?name="example"')
         .expect('Content-Type', /object/)
@@ -60,11 +49,7 @@ test('Greeter with no title', (t) => {
         .end((err, res) => {
             let expected = {error: "Please provide a title!"};
             t.same(res.body, expected, "Provide title error message correct");
-            t.end();
         });
-});
-
-test('Greeter with valid input', (t) => {
     request(app)
         .get('/greeter?name=example name&title=example title')
         .expect('Content-Type', /object/)
@@ -72,11 +57,7 @@ test('Greeter with valid input', (t) => {
         .end((err, res) => {
             let expected = {welcome_message: "Oh, hi there example name, my dear example title!"};
             t.same(res.body, expected, "Returns message correctly");
-            t.end();
         });
-});
-
-test('Greeter with invalid input', (t) => {
     request(app)
         .get('/greeter?name=example name&title=2')
         .expect('Content-Type', /object/)
