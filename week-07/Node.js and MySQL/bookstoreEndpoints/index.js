e960919f35d8b5d3ebfce492e948e27e67ed5329
Wermer Gábor <wermer.gabor@gmail.com>
2018-06-26 16:55:39 +0200

    bookstoreEndpoints init

---
+++ week-07/Node.js and MySQL/bookstoreEndpoints/index.js
@@ -0,0 +1,16 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use('/assets', express.static('./assets'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'/assets/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
