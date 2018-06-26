2f12613e4abce7eb60581f5d6e83023789ccf63a
Wermer Gábor <wermer.gabor@gmail.com>
2018-06-26 12:49:49 +0200

    char list works

--- week-07/XHR/star_wars_api/assets/starwars.js
+++ week-07/XHR/star_wars_api/assets/starwars.js
@@ -1,7 +1,27 @@
+const httpRequest = new XMLHttpRequest();
 const submitButton = document.querySelector('.submitButton');
 const inputField = document.querySelector('.charNameInput');
+const charList = document.querySelector('.charList');
 submitButton.addEventListener('click', searchFor);
 
 function searchFor() {
-    console.log(inputField.value);
+    if (inputField.value) {
+        httpRequest.open('GET', `https://swapi.co/api/people/?search=${inputField.value}`, true);
+        httpRequest.onload = () => {
+            if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+                getCharList(JSON.parse(httpRequest.responseText))
+            }
+        };
+        httpRequest.send();
+    }
+}
+
+
+function getCharList(data) {
+    let results = data.results;
+    for (let result of results) {
+        let charLi = document.createElement('li');
+        charLi.innerHTML = result.name;
+        charList.appendChild(charLi);
+    }
 }
