d796ad3ef92df95a1549b4cdfda167c19a250d17
Wermer Gábor <wermer.gabor@gmail.com>
2018-06-26 14:37:05 +0200

    star wars api done

--- week-07/XHR/star_wars_api/assets/starwars.js
+++ week-07/XHR/star_wars_api/assets/starwars.js
@@ -2,26 +2,69 @@ const httpRequest = new XMLHttpRequest();
 const submitButton = document.querySelector('.submitButton');
 const inputField = document.querySelector('.charNameInput');
 const charList = document.querySelector('.charList');
-submitButton.addEventListener('click', searchFor);
+const movieList = document.querySelector('.movieList');
+submitButton.addEventListener('click', searchForCharacters);
 
-function searchFor() {
+function searchForCharacters() {
     if (inputField.value) {
-        httpRequest.open('GET', `https://swapi.co/api/people/?search=${inputField.value}`, true);
-        httpRequest.onload = () => {
-            if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-                getCharList(JSON.parse(httpRequest.responseText))
-            }
-        };
-        httpRequest.send();
+        getCharactersByName(inputField.value);
     }
 }
 
+function createMovieList(titles) {
+    movieList.innerHTML = "";
+    for (let title of titles) {
+        let movie = document.createElement('li');
+        movie.innerHTML = title;
+        movieList.appendChild(movie);
+    }
+}
 
-function getCharList(data) {
+function getCharactersByName(input) {
+    httpRequest.open('GET', `https://swapi.co/api/people/?search=${input}`, true);
+    httpRequest.onload = () => {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+            createCharList(JSON.parse(httpRequest.responseText));
+        }
+    };
+    httpRequest.send();
+}
+
+function createCharList(data) {
     let results = data.results;
     for (let result of results) {
         let charLi = document.createElement('li');
         charLi.innerHTML = result.name;
+        charLi.addEventListener('click', () => {
+            getMovieLinks(result.name);
+        });
         charList.appendChild(charLi);
     }
+}
+
+function getMovieLinks(tag) {
+    httpRequest.open('GET', `https://swapi.co/api/people/?search=${tag.replace(' ', '%20')}`, false);
+    httpRequest.onload = () => {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+            let data = JSON.parse(httpRequest.responseText);
+            return getMovieTitles(data.results[0].films);
+        }
+    };
+    httpRequest.send();
+}
+
+function getMovieTitles(links) {
+    let movieNames = [];
+    for (let link of links) {
+        httpRequest.open('GET', `${link}`, false);
+        httpRequest.onload = () => {
+            if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+                let data = JSON.parse(httpRequest.responseText);
+                movieNames.push(data.title)
+            }
+        };
+        httpRequest.send();
+    }
+    console.log(movieNames);
+    createMovieList(movieNames);
 }
--- week-07/XHR/star_wars_api/index.js
+++ week-07/XHR/star_wars_api/index.js
@@ -13,3 +13,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+
+setTimeout(() => {
+    console.log('apple'),
+        3000
+});
