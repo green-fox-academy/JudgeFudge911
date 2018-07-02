c71cb7cf249f4f52e014a70ce9513a3c7c3c2495
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-02 22:34:33 +0200

    candygame init

---
+++ week-08/EventListener/CandyGame/game.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const createButton = document.querySelector('.create-candies')
+    const buyButton = document.querySelector('.buy-lollypops')
+    const rainButton = document.querySelector('.candy-machine')
+    function createCandy(){
+
+    }
+
+    function buyLollypop(){
+
+    }
+
+    function generateCandy(){
+
+    }
+
+    //TODO: display Candies/seconds
+
+    function makeItRain(){
+
+    }
+});
