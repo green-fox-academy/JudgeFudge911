15aaf7d687110d43e4cbe153b6a55d8553d364ff
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-02 22:51:21 +0200

    candygame structure

--- week-08/EventListener/CandyGame/game.js
+++ week-08/EventListener/CandyGame/game.js
@@ -1,22 +1,31 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    const createButton = document.querySelector('.create-candies')
-    const buyButton = document.querySelector('.buy-lollypops')
-    const rainButton = document.querySelector('.candy-machine')
-    function createCandy(){
+document.addEventListener("DOMContentLoaded", () => {
+  const createButton = document.querySelector(".create-candies");
+  const buyButton = document.querySelector(".buy-lollypops");
+  const rainButton = document.querySelector(".candy-machine");
+  const lollypops = document.querySelector(".lollypops");
+  const candies = document.querySelector(".candies");
+  let numberOfLollypops = lollypops.childElementCount;
+  let numberOfCandies = candies.innerHTML;
 
+  createButton.addEventListener("click", createCandy);
+  buyButton.addEventListener("click", buyLollypop);
+  rainButton.addEventListener("click", makeItRain);
+
+  function createCandy() {}
+
+  function buyLollypop() {
+      console.log(numberOfCandies);
+
+    if (numberOfCandies >= 10) {
+      lollypops.innerHTML += "\u{1F36D}";
+    } else {
+      console.log("Error: not enough candies to buy lolly");
     }
+  }
 
-    function buyLollypop(){
+  function generateCandy() {}
 
-    }
+  //TODO: display Candies/seconds
 
-    function generateCandy(){
-
-    }
-
-    //TODO: display Candies/seconds
-
-    function makeItRain(){
-
-    }
+  function makeItRain() {}
 });
