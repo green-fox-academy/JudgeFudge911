609a9f96fc245ff0b26810c62fca5e3aed87bb61
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-03 10:29:38 +0200

    OneTimeClicker done

---
+++ week-08/EventListener/OneTimeClicker/app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", logTime);
+
+  function logTime(e) {
+    console.log(e.timeStamp);
+    button.disabled = true;
+    button.removeEventListener("click", logTime);
+  }
+});
