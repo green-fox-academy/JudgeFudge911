a78d9c19df4e6701265799f7d0b7836ccff9de69
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-03 14:47:52 +0200

    move img works

--- week-08/EventListener/Bubbling/app.js
+++ week-08/EventListener/Bubbling/app.js
@@ -6,19 +6,33 @@ document.addEventListener("DOMContentLoaded", () => {
   nav.addEventListener("click", zoomImage);
 
   function moveImage(e) {
-    if (e.target.getAttribute("data-direction") === "up") {
-    } else if (e.target.getAttribute("data-direction") === "down") {
-    } else if (e.target.getAttribute("data-direction") === "left") {
-    } else {
-      //right
+      console.log(img.backgroundPosition);
+
+    if (e.target.dataset.action === "move") {
+      let currentBackgroundPositionX = parseInt(img.style.backgroundPositionX) || 0;
+      let currentBackgroundPositionY = parseInt(img.style.backgroundPositionY) || 0;
+      if (e.target.dataset.direction === "up") {
+        img.style.backgroundPositionY = `${currentBackgroundPositionY - 10}px`;
+      }
+      if (e.target.dataset.direction === "down") {
+        img.style.backgroundPositionY = `${currentBackgroundPositionY + 10}px`;
+      }
+      if (e.target.dataset.direction === "right") {
+        img.style.backgroundPositionX = `${currentBackgroundPositionX + 10}px`;
+      }
+      if (e.target.dataset.direction === "left") {
+        img.style.backgroundPositionX = `${currentBackgroundPositionX - 10}px`;
+      }
     }
   }
 
   function zoomImage(e) {
-    let currentSize = parseInt(img.style.backgroundSize) || 200;
-    img.style.backgroundSize =
-      e.target.dataset.direction === "in"
-        ? `${currentSize + 20}%`
-        : `${currentSize - 20}%`;
+    if (e.target.dataset.action === "zoom") {
+      let currentSize = parseInt(img.style.backgroundSize) || 200;
+      img.style.backgroundSize =
+        e.target.dataset.direction === "in"
+          ? `${currentSize + 20}%`
+          : `${currentSize - 20}%`;
+    }
   }
 });
