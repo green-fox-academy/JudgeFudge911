49820250f84404651f63e87e2bb17dfcf5e59fe6
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-04 12:25:52 +0200

    buttondisable done

--- week-08/Forms/ButtonDisable/app.js
+++ week-08/Forms/ButtonDisable/app.js
@@ -3,8 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const signUpButton = document.querySelector(".signUp");
   const signUpFieldset = document.querySelector(".signUpCatFieldset");
   const loveCatsButton = document.querySelector(".loveCats");
+  const buttonContainer = document.querySelector(".buttonContainer");
+  const viktor = document.querySelector("#Viktor");
+
   petFieldset.addEventListener("change", changeSignup);
   signUpFieldset.addEventListener("change", changeLoveCats);
+  buttonContainer.addEventListener("click", alertSucces);
+
   function changeSignup(e) {
     let value = e.target.value;
     if (value === "Dog" || value === "Cat") {
@@ -16,8 +21,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function changeLoveCats(e) {
     let value = e.target.value;
+    if (value === "No" && viktor.checked) {
+      signUpButton.removeAttribute("disabled");
+    }
     value === "Yes"
       ? loveCatsButton.removeAttribute("disabled")
       : loveCatsButton.setAttribute("disabled", "");
+  }
+
+  function alertSucces(e) {
+    if (loveCatsButton.hasAttribute("disabled")) {
+      alert("Sigh, we still added you to the cat facts list");
+    } else {
+      alert("Thank you, you've succesfully signed up for cat facts.");
+    }
   }
 });
