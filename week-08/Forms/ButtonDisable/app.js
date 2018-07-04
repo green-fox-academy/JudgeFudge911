738066e83ef26f61db9871c9e253a617e23590bc
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-04 12:17:09 +0200

    buttondisable, lovecats

--- week-08/Forms/ButtonDisable/app.js
+++ week-08/Forms/ButtonDisable/app.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const petFieldset = document.querySelector(".favouritePetFieldset");
-  const signUpButton = document.querySelector('.signUp');
-
-  const loveCatsButton = document.querySelector('.loveCats');
+  const signUpButton = document.querySelector(".signUp");
+  const signUpFieldset = document.querySelector(".signUpCatFieldset");
+  const loveCatsButton = document.querySelector(".loveCats");
   petFieldset.addEventListener("change", changeSignup);
-
+  signUpFieldset.addEventListener("change", changeLoveCats);
   function changeSignup(e) {
-      let value = e.target.value;
-    if(value === "Dog" || value === "Cat"){
-        signUpButton.removeAttribute('disabled');
+    let value = e.target.value;
+    if (value === "Dog" || value === "Cat") {
+      signUpButton.removeAttribute("disabled");
     } else {
-        signUpButton.setAttribute('disabled', "");
+      signUpButton.setAttribute("disabled", "");
     }
+  }
+
+  function changeLoveCats(e) {
+    let value = e.target.value;
+    value === "Yes"
+      ? loveCatsButton.removeAttribute("disabled")
+      : loveCatsButton.setAttribute("disabled", "");
   }
 });
