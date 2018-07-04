0384456ba2b4dc09938f82f35ab99d4c9c3b45f8
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-04 13:44:24 +0200

    movieselector works

--- week-08/Forms/MovieSelector/app.js
+++ week-08/Forms/MovieSelector/app.js
@@ -1,16 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   const genre = document.querySelector("#genre");
   const movies = document.querySelector("#movies");
+  const result = document.querySelector(".result");
   genre.addEventListener("change", filterOptions);
+//   movies.addEventListener("change", renderMovieTitle);
 
-  function filterOptions(e) {
-    let value = e.target.value;
+  function filterOptions(e, genre) {
+    let value = genre || e.target.value;
     Array.from(movies).forEach(e => {
       if (!e.classList.contains(value)) {
-        e.setAttribute("disabled", "");
+        e.disabled = true;
       } else {
-        e.removeAttribute("disabled");
+        e.disabled = false;
       }
     });
   }
+
+//   function renderMovieTitle(e) {
+//     let value = e.target.value;
+//     let currentGenre = e.target[e.target.selectedIndex].className;
+
+//     if (genre.value === "0") {
+//       Array.from(genre).forEach(e => {
+//         if (e.className === currentGenre) {
+//           e.selected = true;
+//         } else {
+//             e.selected = false;
+//         }
+//       });
+//     }
+//     result.innerHTML = `The selected movie is: ${value}`;
+//   }
 });
