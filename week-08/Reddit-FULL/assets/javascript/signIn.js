e825872fd14602486d835922f95d1f3b2de60ea2
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-06 12:59:11 +0200

    sign in alerts user about wrong pass

--- week-08/Reddit-FULL/assets/javascript/signIn.js
+++ week-08/Reddit-FULL/assets/javascript/signIn.js
@@ -1,12 +1,12 @@
 window.onload = () => {
-  const submitButton = document.querySelector("#submit");
-  const username = document.querySelector("#usernameInput");
-  const password = document.querySelector("#passwordInput");
-  submitButton.addEventListener("click", submitForm);
+  const submitButton = document.querySelector('#submit');
+  const username = document.querySelector('#usernameInput');
+  const password = document.querySelector('#passwordInput');
+  submitButton.addEventListener('click', submitForm);
 
   let users = [];
 
-  fetch("http://localhost:3000/data/users")
+  fetch('http://localhost:3000/data/users')
     .then(res => res.json())
     .then(data => {
       users = data;
@@ -15,15 +15,22 @@ window.onload = () => {
   function submitForm(e) {
     //TODO run on input field enter keydown event too
     e.preventDefault();
+    let userExist = false;
     users.forEach(element => {
       if (element.name == username.value) {
         if (element.password == password.value) {
-          localStorage.setItem("user", username.value);
-          window.location = "http://localhost:3000/posts";
-        } else {
-          //TODO tell user input is not valid
+          localStorage.setItem('user', username.value);
+          window.location = 'http://localhost:3000/posts';
+          userExist = true;
         }
       }
     });
+    if (userExist) {
+      userExist = false;
+    } else {
+      username.value = '';
+      password.value = '';
+      alert('Username or password is wrong. Please try again');
+    }
   }
 };
