302db428a249c1227074aba3c04beb0292c3afce
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-09 14:38:50 +0200

    classes structure done, render renders floor

--- week-09/Closures & This/Elevator/elevator.js
+++ week-09/Closures & This/Elevator/elevator.js
@@ -1,24 +1,39 @@
-window.onload(() => {
+window.onload = () => {
   const createElevatorController = () => {
-    let view = createElevatorView();
-    let model = createElevatorModel();
-    return () => {
-      function eventHandler(target) {
-        switch (target) {
-          case 'up':
-            model.up();
-            break;
-          case 'down':
-            model.down();
-            break;
-          case 'add':
-            model.add();
-            break;
-          case 'remove':
-            model.remove();
-            break;
-        }
+    const maxFloor = 10;
+    const maxPeople = 10;
+    let view = createElevatorView(maxFloor, maxPeople);
+    let model = createElevatorModel(maxFloor, maxPeople);
+    function eventHandler(target) {
+      switch (target) {
+        case 'up':
+          model.moveUp();
+          break;
+        case 'down':
+          model.moveDown();
+          break;
+        case 'add':
+          model.addPeople();
+          break;
+        case 'remove':
+          model.removePeople();
+          break;
       }
+      view.render();
+    }
+
+    function getModel() {
+      return model;
+    }
+
+    function getView() {
+      return view;
+    }
+
+    return {
+      eventHandler,
+      getModel,
+      getView
     };
   };
 
@@ -26,68 +41,88 @@ window.onload(() => {
     let position = 0;
     let direction = 0;
     let people = 0;
+
     function isValidPeopleOrFloor(isFloor) {
-      return isFloor
-        ? elevatorPosition < maxFloor
-        : peopleInElevator < maxPeople;
+      return isFloor ? position < maxFloor : people < maxPeople;
     }
-    return () => {
-      function addPeople() {
-        if (isValidPeopleOrFloor(false)) {
-          peopleInElevator++;
-          view.render();
-        }
-      }
-      function removePeople() {
-        if (isValidPeopleOrFloor(false)) {
-          peopleInElevator--;
-          view.render();
-        }
-      }
-      function getPosition() {
-        return position;
-      }
-      function moveUp() {
-        if (isValidPeopleOrFloor(true)) {
-          position++;
-          view.render();
-        }
-      }
 
-      function moveDown() {
-        if (isValidPeopleOrFloor(true)) {
-          position--;
-          view.render();
-        }
+    function addPeople() {
+      if (isValidPeopleOrFloor(false)) {
+        people++;
       }
+    }
 
-      function getDirection() {
-        return direction;
+    function removePeople() {
+      if (isValidPeopleOrFloor(false)) {
+        people--;
       }
+    }
 
-      function getPeople() {
-        return people;
+    function getPeople() {
+      return people;
+    }
+
+    function moveUp() {
+      if (isValidPeopleOrFloor(true)) {
+        position++;
       }
+    }
+
+    function moveDown() {
+      if (isValidPeopleOrFloor(true)) {
+        position--;
+      }
+    }
+
+    function getPosition() {
+      return position;
+    }
+
+    function getDirection() {
+      return direction;
+    }
+    return {
+      addPeople,
+      removePeople,
+      getPeople,
+      moveUp,
+      moveDown,
+      getPosition,
+      getDirection
     };
   };
 
-  const createElevatorView = () => {
-    const up = document.querySelector('.up');
-    const down = document.querySelector('.down');
-    const add = document.querySelector('.add');
-    const remove = document.querySelector('.remove');
+  const createElevatorView = (maxFloor, maxPeople) => {
     const elevatorContainer = document.querySelector('.elevatorContainer');
-    up.addEventListener('click', () => {
-      eventHandler('up');
-    });
-    down.addEventListener('click', () => {
-      eventHandler('down');
-    });
-    add.addEventListener('click', () => {
-      eventHandler('add');
-    });
-    remove.addEventListener('click', () => {
-      eventHandler('remove');
-    });
+    function render() {
+      for (let i = 0; i < maxFloor; i++) {
+        let floor = document.createElement('div');
+        floor.classList.add(i, 'floor');
+        elevatorContainer.appendChild(floor);
+      }
+    }
+    return {
+      render
+    };
   };
-});
+
+  let controller = createElevatorController();
+
+  const up = document.querySelector('.up');
+  const down = document.querySelector('.down');
+  const add = document.querySelector('.add');
+  const remove = document.querySelector('.remove');
+
+  up.addEventListener('click', () => {
+    controller.eventHandler('up');
+  });
+  down.addEventListener('click', () => {
+    controller.eventHandler('down');
+  });
+  add.addEventListener('click', () => {
+    controller.eventHandler('add');
+  });
+  remove.addEventListener('click', () => {
+    controller.eventHandler('remove');
+  });
+};
