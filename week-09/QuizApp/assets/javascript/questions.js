5ed005ea22f5e7ed1cbb92a949c6762a0efa5232
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-12 17:04:00 +0200

    question remove works

--- week-09/QuizApp/assets/javascript/questions.js
+++ week-09/QuizApp/assets/javascript/questions.js
@@ -1,5 +1,37 @@
 window.onload = () => {
+  const questionContainer = document.querySelector('.questionsContainer');
 
-    
+  questionContainer.addEventListener('click', deleteQuestion);
 
+  fetch('http://localhost:3000/questions')
+    .then(res => res.json())
+    .then(data => renderQuestions(data));
+
+  function renderQuestions(data) {
+    for (const question of data) {
+      let questionCard = document.createElement('div');
+      let currQuestion = document.createElement('p');
+      currQuestion.textContent = question.question;
+      let deleteButton = document.createElement('button');
+      deleteButton.textContent = 'DELETE';
+      questionCard.classList.add('questionCard');
+      questionCard.appendChild(currQuestion);
+      questionCard.appendChild(deleteButton);
+      questionCard.id = question.id;
+      questionContainer.appendChild(questionCard);
+    }
+  }
+
+  function deleteQuestion(e) {
+    if (e.target.tagName.toLowerCase() === 'button') {
+      removeFromDatabase(e.target.parentNode.id);
+      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    }
+  }
+
+  function removeFromDatabase(id) {
+    fetch(`http://localhost:3000/questions/${id}`, {
+      method: 'DELETE'
+    });
+  }
 };
