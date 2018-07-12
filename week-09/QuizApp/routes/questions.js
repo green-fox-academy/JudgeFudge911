92b0ce0bd036af52548a9073ceeec49e780ad2d1
Wermer Gábor <wermer.gabor@gmail.com>
2018-07-12 13:43:46 +0200

    insert into works

--- week-09/QuizApp/routes/questions.js
+++ week-09/QuizApp/routes/questions.js
@@ -18,22 +18,55 @@ router.get('/', (req, res) => {
         Error:
           'There was an error while getting the questions from the database'
       });
+      return;
     }
     res.status(200).json(data);
   });
 });
 
 router.post('/', (req, res) => {
-  const sql = 'INSERT INTO questions';
+  let sqlInsertQuestion = `BEGIN;
+  INSERT INTO questions (question)
+  VALUES (?);
+  INSERT INTO answers (question_id, answer, is_correct)
+  VALUES
+    (LAST_INSERT_ID(), ?, ?),
+    (LAST_INSERT_ID(), ?, ?),
+    (LAST_INSERT_ID(), ?, ?),
+    (LAST_INSERT_ID(), ?, ?);
+  COMMIT;`;
+  const values = [
+    req.body.question,
+    req.body.answer1.answer,
+    req.body.answer1.is_correct,
+    req.body.answer2.answer,
+    req.body.answer2.is_correct,
+    req.body.answer3.answer,
+    req.body.answer3.is_correct,
+    req.body.answer4.answer,
+    req.body.answer4.is_correct
+  ];
+  conn.query(sqlInsertQuestion, values, (err, data) => {
+    if (err) {
+      res.status(500).json({
+        Error: 'There was an error while inserting new question to the database'
+      });
+      return;
+    }
+    res.status(200).json({
+      message: 'Question was added.'
+    });
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  const sqlDeleteQuestion = `DELETE * FROM questions WHERE id=${req.params.id}`;
+  const sqlDeleteQuestion = `DELETE FROM questions WHERE id=${req.params.id}`;
   conn.query(sqlDeleteQuestion, (err, deletedQuestion) => {
     if (err) {
       res.status(500).json({
         Error: 'There was an error while deleting question from the database'
       });
+      return;
     }
     res.status(204);
   });
