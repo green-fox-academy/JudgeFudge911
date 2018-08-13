cc31c1b576b3ac7e7b5051ef8663dc7424519f4c
gabor <wermer.gabor@gmail.com>
2018-08-13 15:07:26 +0200

    routing

--- Angular/QuizGameDemo/Frontend/src/app/app-routing.module.ts
+++ Angular/QuizGameDemo/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'game', pathMatch: 'full'},
+  {path: 'game', component: GameComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
