bfd2c0216be9f3df2df21857cd677c16a3c1d216
gabor <wermer.gabor@gmail.com>
2018-08-13 11:00:52 +0200

    demo init

--- Angular/QuizGameDemo/Frontend/src/app/app-routing.module.ts
+++ Angular/QuizGameDemo/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GameComponent } from './components/game/game.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [];
 
